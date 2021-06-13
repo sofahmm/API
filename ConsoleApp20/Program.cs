3b59b200556708c67598f6f7c8002b9d0382a088
sofahmm <74815523+sofahmm@users.noreply.github.com>
2021-06-13 22:52:55 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,43 @@
+﻿// OMdB
+// KSofia 220
+
+using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+using lib_pars;
+
+namespace OpenWeather
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var apikey = "9ee77822";
+            var movie = "Split";
+            var url = $"http://www.omdbapi.com/?apikey={apikey}&t={movie}";
+
+            var request = WebRequest.Create(url);
+
+            var response = request.GetResponse();
+            var httpStatusCode = (response as HttpWebResponse).StatusCode;
+
+            if (httpStatusCode != HttpStatusCode.OK)
+            {
+                Console.WriteLine(httpStatusCode);
+                return;
+            }
+
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                string result = streamReader.ReadToEnd();
+                Console.WriteLine(result);
+                var OMdB = JsonConvert.DeserializeObject<Root>(result);
+                Console.WriteLine(OMdB.Website);
+            }
+
+        }
+        
+    }
+    
+}
